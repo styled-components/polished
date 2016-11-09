ce878633cee4d25175b9c92c4902425f98c968a4
Brian Hough <bhough@bleacherreport.com>
2016-11-08 22:00:46 -0800

    chore(docs): Watch tasks, examples, them.

--- src/helpers/StripUnit.js
+++ src/helpers/StripUnit.js
@@ -1,13 +1,26 @@
-/**
- * Strips the unit from a given value.
- * @name StripUnit
- * @param {String} value - a string that is a number plus a unit of measure.
- * @returns {Number} - an integer without the unit.
- */
-
+/** @module Helpers */
 import isInteger from '../internalHelpers/_isInteger'
 
-export default function(value) {
+/**
+ * CSS to represent truncated text with an ellipsis.
+ * @static
+ * @param {string} [width = 100%] - Max-width for text to respect before being truncated.
+ * @return {Object} CSS as JS for truncating text.
+ * @example
+ * element {
+ *   '--dimension': stripUnit(100px)
+ * }
+ *
+ * // CSS in JS Output
+ *
+ * element {
+ *   '--dimension': 100
+ * }
+ */
+
+export function stripUnit(value) {
   const unitlessValue = parseFloat(value)
   if (isInteger(unitlessValue)) return unitlessValue
 }
+
+export default stripUnit
--- src/mixins/Ellipsis.js
+++ src/mixins/Ellipsis.js
@@ -1,17 +1,18 @@
+/** @module Mixins */
+
 /**
- * Represents truncated text with an ellipsis.
- * @name Ellipsis
- * @param {String} [width = 100%]
- *   Max-width for text to respect before being truncated.
+ * CSS to represent truncated text with an ellipsis.
+ * @static
+ * @param {string} [width = 100%] - Max-width for text to respect before being truncated.
+ * @return {Object} CSS as JS.
+ * @example
+ * div {
+ *   ellipsis(250px)
+ * }
  *
- * @returns {Object}
- *   CSS as JS for truncating text.
+ * // CSS as JS Output
  *
- * @example Usage with param
- *   Ellipsis(250px)
- *
- * @example Output with param
- * {
+ * div {
  *   'display': 'inline-block',
  *   'max-width': '250px',
  *   'overflow': 'hidden',
@@ -19,23 +20,9 @@
  *   'white-space': 'nowrap',
  *   'word-wrap': 'normal'
  * }
- *
- * @example Usage with default
- *   Ellipsis()
- *
- * @example Output with default
- * {
- *   'display': 'inline-block',
- *   'max-width': '100%',
- *   'overflow': 'hidden',
- *   'text-overflow': 'ellipsis',
- *   'white-space': 'nowrap',
- *   'word-wrap': 'normal'
- * }
- *
  */
 
-export default function Ellipsis(width = '100%') {
+function ellipsis(width = '100%') {
   return {
     'display': 'inline-block',
     'max-width': width,
@@ -45,3 +32,5 @@ export default function Ellipsis(width = '100%') {
     'word-wrap': 'normal'
   }
 }
+
+export default ellipsis
