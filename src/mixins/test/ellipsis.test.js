06bafec5826c13969098a976d21377f38c51510a
Brian Hough <bhough@bleacherreport.com>
2016-11-14 11:32:53 -0800

    chore(mixins): Added ellipsis mixin
    Added ellipsis mixin and associated tests. Also added coverage to .gitignore and dependencies for
    
    handling spreads in babel.
    
    https://github.com/styled-components/polished/issues/1

---
+++ src/mixins/test/ellipsis.test.js
@@ -0,0 +1,21 @@
+import ellipsis from '../ellipsis'
+
+describe('ellipsis', function() {
+
+  it('should pass parameter to the value of max-width', function() {
+    expect({...ellipsis('500px')}).toMatchSnapshot()
+  })
+
+  it('should properly add rules when block has existing rules', function() {
+    expect({
+      background: 'red',
+      ...ellipsis('500px')
+    }).toMatchSnapshot()
+  })
+
+
+  it('should default max-width to 100%', function() {
+    expect({...ellipsis()}).toMatchSnapshot()
+  })
+
+})
