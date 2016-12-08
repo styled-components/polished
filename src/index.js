a054cd17e71052fd17b243bad99c3dadc3493d0c
Brian Hough <brian.r.hough@gmail.com>
2016-12-08 14:34:23 -0800

    chore(selection): Add selection mixin
    Add selection mixin that allows styling of selection in all browsers.
    
    n/a

--- src/index.js
+++ src/index.js
@@ -5,22 +5,31 @@ import stripUnit from './helpers/stripUnit'
 // Mixins
 import clearFix from './mixins/clearFix'
 import ellipsis from './mixins/ellipsis'
+<<<<<<< HEAD
 import hideText from './mixins/hideText'
 import hiDIPI from './mixins/hiDPI'
 import timingFunctions from './mixins/timingFunctions'
 import wordWrap from './mixins/wordWrap'
+=======
+import selection from './mixins/selection'
+>>>>>>> f42fb97... chore(selection): Add selection mixin
 
 const polished = {
   clearFix,
   ellipsis,
+<<<<<<< HEAD
   hideText,
   hiDIPI,
+=======
+  selection,
+>>>>>>> f42fb97... chore(selection): Add selection mixin
   stripUnit,
   timingFunctions,
   wordWrap,
 }
 
 export default polished
+<<<<<<< HEAD
 
 export {
   clearFix,
@@ -30,4 +39,11 @@ export {
   stripUnit,
   timingFunctions,
   wordWrap,
+=======
+export {
+  clearFix,
+  ellipsis,
+  selection,
+  stripUnit
+>>>>>>> f42fb97... chore(selection): Add selection mixin
 }
---
+++ src/mixins/selection.js
@@ -0,0 +1,13 @@
+// @flow
+function selection(styles: Object, parent: string = '') {
+  return {
+    [`${parent}::-moz-selection`]: {
+      ...styles,
+    },
+    [`${parent}::selection`]: {
+      ...styles,
+    },
+  }
+}
+
+export default selection
