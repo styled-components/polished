e179ff7881b1812bdaa5b58e26c896b3df48b6b7
Brian Hough <brian.r.hough@gmail.com>
2016-12-08 07:03:16 -0800

    chore(package): Export new methods
    Export new mixins that were created.
    
    n/a

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import stripUnit from './helpers/stripUnit'
 // Mixins
 import clearFix from './mixins/clearFix'
 import ellipsis from './mixins/ellipsis'
+import hideText from './mixins/hideText'
+import hiDIPI from './mixins/hiDPI'
+import timingFunctions from './mixins/timingFunctions'
+import wordWrap from './mixins/wordWrap'
 
 const polished = {
   clearFix,
   ellipsis,
+  hideText,
+  hiDIPI,
   stripUnit,
+  timingFunctions,
+  wordWrap
 }
 
 export default polished
-export { clearFix, ellipsis, stripUnit }
+
+export {
+  clearFix,
+  ellipsis,
+  hideText,
+  hiDIPI,
+  stripUnit,
+  timingFunctions,
+  wordWrap
+}
