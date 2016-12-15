0b0286e302d94b1555dd4372d206aba1ce5249c5
Max <contact@mstoiber.com>
2016-12-15 15:13:04 +0100

    chore(docs): fix linting issues in main theme file

--- docs-theme/index.js
+++ docs-theme/index.js
@@ -1,84 +1,82 @@
-'use strict';
-
-var fs = require('fs'),
-  path = require('path'),
-  File = require('vinyl'),
-  vfs = require('vinyl-fs'),
-  _ = require('lodash'),
-  concat = require('concat-stream'),
-  GithubSlugger = require('github-slugger'),
-  createFormatters = require('documentation').util.createFormatters,
-  createLinkerStack = require('documentation').util.createLinkerStack,
-  hljs = require('highlight.js');
+const fs = require('fs')
+const path = require('path')
+const File = require('vinyl')
+const vfs = require('vinyl-fs')
+const _ = require('lodash')
+const concat = require('concat-stream')
+const GithubSlugger = require('github-slugger')
+const createFormatters = require('documentation').util.createFormatters
+const createLinkerStack = require('documentation').util.createLinkerStack
+const hljs = require('highlight.js')
 
 module.exports = function (comments, options, callback) {
+  const linkerStack = createLinkerStack(options)
+    .namespaceResolver(comments, (namespace) => {
+      const slugger = new GithubSlugger()
+      return `#${slugger.slug(namespace)}`
+    })
 
-  var linkerStack = createLinkerStack(options)
-    .namespaceResolver(comments, function (namespace) {
-      var slugger = new GithubSlugger();
-      return '#' + slugger.slug(namespace);
-    });
+  const formatters = createFormatters(linkerStack.link)
 
-  var formatters = createFormatters(linkerStack.link);
+  hljs.configure(options.hljs || {})
 
-  hljs.configure(options.hljs || {});
-
-  var sharedImports = {
+  const sharedImports = {
     imports: {
-      slug: function (str) {
-        var slugger = new GithubSlugger();
-        return slugger.slug(str);
+      slug (str) {
+        const slugger = new GithubSlugger()
+        return slugger.slug(str)
       },
-      shortSignature: function (section) {
-        var prefix = '';
+      shortSignature (section) {
+        let prefix = ''
         if (section.kind === 'class') {
-          prefix = 'new ';
+          prefix = 'new '
         } else if (section.kind !== 'function') {
-          return section.name;
+          return section.name
         }
-        return prefix + section.name + formatters.parameters(section, true);
+        return prefix + section.name + formatters.parameters(section, true)
       },
-      signature: function (section) {
-        var returns = '';
-        var prefix = '';
+      signature (section) {
+        let returns = ''
+        let prefix = ''
         if (section.kind === 'class') {
-          prefix = 'new ';
+          prefix = 'new '
         } else if (section.kind !== 'function') {
-          return section.name;
+          return section.name
         }
         if (section.returns) {
-          returns = ': ' +
-            formatters.type(section.returns[0].type);
+          returns = `: ${
+            formatters.type(section.returns[0].type)}`
         }
-        return prefix + section.name + formatters.parameters(section) + returns;
+        return prefix + section.name + formatters.parameters(section) + returns
       },
-      md: function (ast, inline) {
+      md (ast, inline) {
+        let newAst = ast
         if (inline && ast && ast.children.length && ast.children[0].type === 'paragraph') {
-          ast = {
+          newAst = {
             type: 'root',
-            children: ast.children[0].children.concat(ast.children.slice(1))
-          };
+            children: ast.children[0].children.concat(ast.children.slice(1)),
+          }
         }
-        return formatters.markdown(ast);
+        return formatters.markdown(newAst)
       },
       formatType: formatters.type,
       autolink: formatters.autolink,
-      highlight: function (example) {
+      highlight (example) {
         if (options.hljs && options.hljs.highlightAuto) {
-          return hljs.highlightAuto(example).value;
+          return hljs.highlightAuto(example).value
         }
-        return hljs.highlight('js', example).value;
-      }
-    }
-  };
+        return hljs.highlight('js', example).value
+      },
+    },
+  }
 
-  sharedImports.imports.renderSectionList =  _.template(fs.readFileSync(path.join(__dirname, 'partials/section_list._'), 'utf8'), sharedImports);
-  sharedImports.imports.renderSection = _.template(fs.readFileSync(path.join(__dirname, 'partials/section._'), 'utf8'), sharedImports);
+  sharedImports.imports.renderSectionList = _.template(fs.readFileSync(path.join(__dirname, 'partials/section_list._'), 'utf8'), sharedImports)
+  sharedImports.imports.renderSection = _.template(fs.readFileSync(path.join(__dirname, 'partials/section._'), 'utf8'), sharedImports)
   sharedImports.imports.renderNote = _.template(fs.readFileSync(path.join(__dirname, 'partials/note._'), 'utf8'), sharedImports)
   sharedImports.imports.renderDocs = _.template(fs.readFileSync(path.join(__dirname, 'docs/index._'), 'utf8'), sharedImports)
   sharedImports.imports.renderHome = _.template(fs.readFileSync(path.join(__dirname, 'index._'), 'utf8'), sharedImports)
 
-  var mainTemplate = _.template(fs.readFileSync(path.join(__dirname, 'partials/base._'), 'utf8'), sharedImports)
+  const mainTemplate = _.template(fs.readFileSync(path.join(__dirname, 'partials/base._'), 'utf8'), sharedImports)
 
   const pages = [{
     path: 'index.html',
@@ -90,17 +88,17 @@ module.exports = function (comments, options, callback) {
   }]
 
   // push assets into the pipeline as well.
-  vfs.src([__dirname + '/assets/**'], { base: __dirname })
-    .pipe(concat(function (files) {
-      callback(null, files.concat(pages.map(function (page) {
+  vfs.src([`${__dirname}/assets/**`], { base: __dirname })
+    .pipe(concat((files) => {
+      callback(null, files.concat(pages.map((page) => {
         const data = Object.assign({}, {
-          options: options
+          options,
         }, page.data)
         const compiled = mainTemplate(data)
         return new File({
           path: page.path,
-          contents: new Buffer(compiled, 'utf8')
+          contents: new Buffer(compiled, 'utf8'),
         })
-      })));
-    }));
-};
+      })))
+    }))
+}
