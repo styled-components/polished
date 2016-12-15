7ec897a34acf3f0cc11ec3e795b2c01f7705b750
Nik Graf <nik@nikgraf.com>
2016-12-15 12:27:25 +0100

    chore(rgb): add first draft of rgb

--- src/helpers/rgb.js
+++ src/helpers/rgb.js
@@ -6,14 +6,12 @@
  * @example
  * // Styles as object usage
  * const styles = {
- *   background: rgb('255, 205, 100'),
  *   background: rgb(255, 205, 100),
  *   background: rgb({ red: 255, green: 205, blue: 100 }),
  * }
  *
  * // styled-components usage
  * const div = styled.div`
- *   background: ${rgb('255, 205, 100')};
  *   background: ${rgb(255, 205, 100)};
  *   background: ${rgb({ red: 255, green: 205, blue: 100 })};
  * `
@@ -21,7 +19,8 @@
  * // CSS in JS Output
  *
  * element {
- *   background: TODO
+ *   background: "#ffcd64";
+ *   background: "#ffcd64";
  * }
  */
 
@@ -31,8 +30,19 @@ type RgbColor = {
   blue: number,
 }
 
-function rgb(value: string | RgbColor | number, green?: number, blue?: number): string {
-  return '#FFF'
+function toHex(value) {
+  const hex = value.toString(16)
+  return hex.length === 1 ? `0${hex}` : hex
+}
+
+function rgb(value: RgbColor | number, green?: number, blue?: number): string {
+  if (typeof value === 'number' && typeof green === 'number' && typeof blue === 'number') {
+    return `#${toHex(value)}${toHex(green)}${toHex(blue)}`
+  } else if (typeof value === 'object' && green === undefined && blue === undefined) {
+    return `#${toHex(value.red)}${toHex(value.green)}${toHex(value.blue)}`
+  }
+
+  throw new Error('Passed invalid arguments to rgb, please pass multiple numbers e.g. rgb(255, 205, 100) or an object e.g. rgb({ red: 255, green: 205, blue: 100 }).')
 }
 
 export default rgb
--- src/helpers/test/rgb.test.js
+++ src/helpers/test/rgb.test.js
@@ -1,8 +1,17 @@
 // @flow
 import rgb from '../rgb'
 
-describe('em', () => {
-  it('should convert a rgb string to a hex color', () => {
-    expect({ background: rgb('255, 205, 100') }).toMatchSnapshot()
+describe('rgb', () => {
+  it('should convert multiple numbers to a hex color', () => {
+    expect({ background: rgb(255, 205, 100) }).toMatchSnapshot()
+  })
+
+  it('should convert a rgb object to a hex color', () => {
+    expect({ background: rgb({ red: 255, green: 205, blue: 100 }) }).toMatchSnapshot()
+  })
+
+  it('should throw an error if an object and multiple arguments are passed', () => {
+    expect(() => ({ background: rgb({ red: 255, green: 1, blue: 1 }, 250, 100) }))
+      .toThrow('Passed invalid arguments to rgb, please pass multiple numbers e.g. rgb(255, 205, 100) or an object e.g. rgb({ red: 255, green: 205, blue: 100 }).')
   })
 })
