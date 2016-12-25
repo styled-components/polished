741d8df0ef9b75509e0e216cfe6529d5f4e6281a
Nik Graf <nik@nikgraf.com>
2016-12-25 22:28:53 +0100

    chore(rgba): add rgba

---
+++ src/helpers/rgba.js
@@ -0,0 +1,23 @@
+// @flow
+
+type RgbaColor = {
+  red: number,
+  green: number,
+  blue: number,
+  alpha: number,
+}
+
+function rgba(value: RgbaColor | number, green?: number, blue?: number, alpha?: number): string {
+  if (typeof value === 'number' &&
+      typeof green === 'number' &&
+      typeof blue === 'number' &&
+      typeof alpha === 'number') {
+    return `rgba(${value},${green},${blue},${alpha})`
+  } else if (typeof value === 'object' && green === undefined && blue === undefined && alpha === undefined) {
+    return `rgba(${value.red},${value.green},${value.blue},${value.alpha})`
+  }
+
+  throw new Error('Passed invalid arguments to rgba, please pass multiple numbers e.g. rgb(255, 205, 100, 180) or an object e.g. rgb({ red: 255, green: 205, blue: 100, alpha: 180 }).')
+}
+
+export default rgba
---
+++ src/helpers/test/rgba.test.js
@@ -0,0 +1,17 @@
+// @flow
+import rgba from '../rgba'
+
+describe('rgb', () => {
+  it('should convert multiple numbers to a rgba string', () => {
+    expect({ background: rgba(255, 205, 100, 180) }).toMatchSnapshot()
+  })
+
+  it('should convert a rgba object to a hex color', () => {
+    expect({ background: rgba({ red: 255, green: 205, blue: 100, alpha: 180 }) }).toMatchSnapshot()
+  })
+
+  it('should throw an error if an object and multiple arguments are passed', () => {
+    expect(() => ({ background: rgba({ red: 255, green: 1, blue: 1, alpha: 180 }, 250, 100) }))
+      .toThrow('Passed invalid arguments to rgba, please pass multiple numbers e.g. rgb(255, 205, 100, 180) or an object e.g. rgb({ red: 255, green: 205, blue: 100, alpha: 180 }).')
+  })
+})
