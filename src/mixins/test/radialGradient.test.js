7c5ac472111a594fdaad437bea9abc24f6df00e0
bhough <brian.r.hough@gmail.com>
2017-01-04 23:10:27 -0800

    chore(radialGradient): Radial Gradient mixin
    Mixin for creating a radial gradient and setting a background fallback
    
    Addresses item in #1

---
+++ src/mixins/test/radialGradient.test.js
@@ -0,0 +1,54 @@
+// @flow
+import radialGradient from '../radialGradient'
+
+describe('radialGradient', function() {
+  it('returns the correct object when only passed two color stops, including parsed fallback with no percentage', function() {
+    expect({...radialGradient({
+      colorStops: ['#fff', '#000'],
+    })}).toMatchSnapshot()
+  })
+
+  it('returns the correct object when passed extent, shape, and position, including parsed fallback with percentage', function() {
+    expect({...radialGradient({
+      colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+      extent: 'farthest-corner at 45px 45px',
+      position: 'center',
+      shape: 'ellipse',
+    })}).toMatchSnapshot()
+  })
+
+  it('returns the correct object when passed extent and shape', function() {
+    expect({...radialGradient({
+      colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+      extent: 'farthest-corner at 45px 45px',
+      shape: 'ellipse',
+    })}).toMatchSnapshot()
+  })
+
+  it('returns the correct object when passed just extent', function() {
+    expect({...radialGradient({
+      colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+      extent: 'farthest-corner at 45px 45px',
+    })}).toMatchSnapshot()
+  })
+
+  it('properly overrides the fallback when it is passed', function() {
+    expect({...radialGradient({
+      colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+      extent: 'farthest-corner at 45px 45px',
+      fallback: '#FFF',
+    })}).toMatchSnapshot()
+  })
+
+ it('should throw an error when not provided at least 2 color-stops', function() {
+    expect(() => {
+      radialGradient(
+        {
+          colorStops: ['#00FFFF 0%'],
+          extent: 'farthest-corner at 45px 45px',
+          fallback: '#FFF',
+        }
+      )
+    }).toThrow('radialGradient requries at least 2 color-stops to properly render.')
+  })
+})
