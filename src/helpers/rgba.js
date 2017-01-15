d3e996ff09a1314eaaff83691265959ad4e27b15
Nik Graf <nik@nikgraf.com>
2017-01-15 05:36:36 -0800

    chore(rgba): fix rgba behaviour

--- src/helpers/rgba.js
+++ src/helpers/rgba.js
@@ -15,23 +15,23 @@ type RgbaColor = {
  * @example
  * // Styles as object usage
  * const styles = {
- *   background: rgba(255, 205, 100, 180),
- *   background: rgba({ red: 255, green: 205, blue: 100, alpha: 180 }),
- *   background: rgba(255, 205, 100, 255),
+ *   background: rgba(255, 205, 100, 0.7),
+ *   background: rgba({ red: 255, green: 205, blue: 100, alpha: 0.7 }),
+ *   background: rgba(255, 205, 100, 1),
  * }
  *
  * // styled-components usage
  * const div = styled.div`
- *   background: ${rgba(255, 205, 100, 180)};
- *   background: ${rgba({ red: 255, green: 205, blue: 100, alpha: 180 })};
- *   background: ${rgba(255, 205, 100, 255)};
+ *   background: ${rgba(255, 205, 100, 0.7)};
+ *   background: ${rgba({ red: 255, green: 205, blue: 100, alpha: 0.7 })};
+ *   background: ${rgba(255, 205, 100, 1)};
  * `
  *
  * // CSS in JS Output
  *
  * element {
- *   background: "rgba(255,205,100,180)";
- *   background: "rgba(255,205,100,180)";
+ *   background: "rgba(255,205,100,0.7)";
+ *   background: "rgba(255,205,100,0.7)";
  *   background: "#ffcd64";
  * }
  */
@@ -40,14 +40,14 @@ function rgba(value: RgbaColor | number, green?: number, blue?: number, alpha?: 
       typeof green === 'number' &&
       typeof blue === 'number' &&
       typeof alpha === 'number') {
-    return alpha === 255 ? rgb(value, green, blue) : `rgba(${value},${green},${blue},${alpha})`
+    return alpha >= 1 ? rgb(value, green, blue) : `rgba(${value},${green},${blue},${alpha})`
   } else if (typeof value === 'object' && green === undefined && blue === undefined && alpha === undefined) {
-    return value.alpha === 255
+    return value.alpha >= 1
       ? rgb(value.red, value.green, value.blue)
       : `rgba(${value.red},${value.green},${value.blue},${value.alpha})`
   }
 
-  throw new Error('Passed invalid arguments to rgba, please pass multiple numbers e.g. rgb(255, 205, 100, 180) or an object e.g. rgb({ red: 255, green: 205, blue: 100, alpha: 180 }).')
+  throw new Error('Passed invalid arguments to rgba, please pass multiple numbers e.g. rgb(255, 205, 100, 0.75) or an object e.g. rgb({ red: 255, green: 205, blue: 100, alpha: 0.75 }).')
 }
 
 export default rgba
