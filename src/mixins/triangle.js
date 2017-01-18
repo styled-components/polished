a8e1c38be5d48c9b33d80ea02eb8e5e821922e6e
Martin <martinmachycek@Martins-MacBook-Pro-3.local>
2017-01-18 21:32:51 +1100

    chore(triangle): Added getBorderWidth function

--- src/mixins/triangle.js
+++ src/mixins/triangle.js
@@ -1,4 +1,6 @@
 // @flow
+/* eslint-disable default-case, consistent-return */
+
 
 /**
  * CSS to represent triangle with any pointing direction.
@@ -7,15 +9,13 @@
  * // Styles as object usage
  *
  * const styles = {
- *   ...triangle({ pointing: 'right', width: '100px', height: '100px'})
+ *   ...triangle({ pointing: 'right', width: '100px', height: '100px', color: 'red' })
  * }
- * const styles = {
- *   ...triangle({ pointing: 'right', width: '100px', height: '100px'})
- * }
+ *
  *
  * // styled-components usage
  * const div = styled.div`
- *   ${triangle({ pointing: 'right', width: '100px', height: '100px'})}
+ *   ${triangle({ pointing: 'right', width: '100px', height: '100px', color: 'red' })}
  *
  *
  * // CSS as JS Output
@@ -32,86 +32,47 @@
 
 type PointingDirection = 'top' | 'right' | 'bottom' | 'left'
 
-type TriangleArgs = {
+type BorderWidthArgs = {
+  height: number,
+  width: number,
   pointingDirection: PointingDirection,
+}
+
+type TriangleArgs = BorderWidthArgs & {
   color: string,
-  height: number,
-  width: number,
 }
 
-type TriangleSizes = {
-  height: number,
-  width: number,
-}
-
-const getLeftPointingTriangle = ({ height, width } : TriangleSizes) => (
-  `${height}px ${width}px ${height}px 0`
-)
-
-const getRightPointingTriangle = ({ height, width } : TriangleSizes) => (
-  `${height}px 0 ${height}px ${width}px`
-)
-
-const getTopPointingTriangle = ({ height, width } : TriangleSizes) => (
-  `0 ${width}px ${height}px ${width}px`
-)
-
-const getBottomPointingTriangle = ({ height, width } : TriangleSizes) => (
-  `${height}px ${width}px 0 ${width}px`
-)
-
-function triangle({ pointingDirection, width, height, color } : TriangleArgs) {
-  let borderWidth
-
-  // check if arrow is in horizontal direction
-  if (pointingDirection === 'left' || pointingDirection === 'right') {
-    const sizes = {
-      width,
-      height: height / 2,
-    }
-
-    if (pointingDirection === 'left') {
-      borderWidth = getLeftPointingTriangle(sizes)
-    } else {
-      borderWidth = getRightPointingTriangle(sizes)
-    }
-  } else {
-    const sizes = {
-      width: width / 2,
-      height,
-    }
-
-    if (pointingDirection === 'top') {
-      borderWidth = getTopPointingTriangle(sizes)
-    } else {
-      borderWidth = getBottomPointingTriangle(sizes)
-    }
-  }
-
-
-  // needed for border-color
-  const reverseDirection = {
-    left: 'right',
-    right: 'left',
-    top: 'bottom',
-    bottom: 'top',
-  }
-
-  return {
-    'border-color': 'transparent',
-    'width': '0',
-    'height': '0',
-    'border-width': borderWidth,
-    'border-style': 'solid',
-
-    /*
-    * javascript Object sorting orders 'border-color' after 'border-bottom-color'
-    * (bottom-b) is before (bottom-c) - !important is needed
-    * { border-bottom-color: 'red', border-color: 'transparent' }
-    */
-
-    [`border-${reverseDirection[pointingDirection]}-color`]: `${color} !important`,
+const getBorderWidth = ({ pointingDirection, height, width } : BorderWidthArgs) => {
+  switch (pointingDirection) {
+    case 'top': return `0 ${width / 2}px ${height}px ${width / 2}px`
+    case 'left': return `${height / 2}px ${width}px ${height / 2}px 0`
+    case 'bottom': return `${height}px ${width / 2}px 0 ${width / 2}px`
+    case 'right': return `${height / 2}px 0 ${height / 2}px ${width}px`
   }
 }
+
+// needed for border-color
+const reverseDirection = {
+  left: 'right',
+  right: 'left',
+  top: 'bottom',
+  bottom: 'top',
+}
+
+const triangle = ({ pointingDirection, width, height, color } : TriangleArgs) => ({
+  'border-color': 'transparent',
+  'width': '0',
+  'height': '0',
+  'border-width': getBorderWidth({ height, width, pointingDirection }),
+  'border-style': 'solid',
+
+  /*
+  * javascript Object sorting orders 'border-color' after 'border-bottom-color'
+  * (bottom-b) is before (bottom-c) - !important is needed
+  * { border-bottom-color: 'red', border-color: 'transparent' }
+  */
+
+  [`border-${reverseDirection[pointingDirection]}-color`]: `${color} !important`,
+})
 
 export default triangle
