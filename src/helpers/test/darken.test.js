0f53f7439b9e23f2476d81f261d3ae4b00c6c036
Nik Graf <nik@nikgraf.com>
2017-02-12 19:13:32 +0100

    chore(darken): add another test

--- src/helpers/test/darken.test.js
+++ src/helpers/test/darken.test.js
@@ -6,7 +6,11 @@ describe('rgb', () => {
     expect(darken(0.2, '#444')).toMatchSnapshot()
   })
 
-  it('should darken a color with opacity by 30%', () => {
-    expect(darken(0.1, 'rgba(260,140,140,0.7)')).toMatchSnapshot()
+  it('should darken a typical color with opacity by 10%', () => {
+    expect(darken(0.1, 'rgba(220,140,140,0.7)')).toMatchSnapshot()
+  })
+
+  it('should darken a color with a value of 255 and opacity by 10%', () => {
+    expect(darken(0.1, 'rgba(255,140,140,0.7)')).toMatchSnapshot()
   })
 })
