151305bb0afee0d48a3bbfad9fbf52bdb63ae306
Nik Graf <nik@nikgraf.com>
2017-02-12 19:18:47 +0100

    chore(darken): add a test to not go below 0

--- src/helpers/test/darken.test.js
+++ src/helpers/test/darken.test.js
@@ -13,4 +13,8 @@ describe('rgb', () => {
   it('should darken a color with a value of 255 and opacity by 10%', () => {
     expect(darken(0.1, 'rgba(255,140,140,0.7)')).toMatchSnapshot()
   })
+
+  it('should darken a color but not go below 0', () => {
+    expect(darken(0.8, 'rgba(40,20,10,0.7)')).toMatchSnapshot()
+  })
 })
