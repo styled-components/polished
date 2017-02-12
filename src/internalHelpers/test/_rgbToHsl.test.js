9d1d4ab77ad8e7bd91e301a8302f56890d9d5ec6
Nik Graf <nik@nikgraf.com>
2017-02-12 15:06:01 +0100

    chore(internalHelpers): add test for rgbToHsl

--- src/internalHelpers/test/_rgbToHsl.test.js
+++ src/internalHelpers/test/_rgbToHsl.test.js
@@ -11,6 +11,10 @@ describe('hslToHex', () => {
     expect({ background: rgbToHsl(0, 0, 255) }).toMatchSnapshot()
   })
 
+  it('should convert the color green', () => {
+    expect({ background: rgbToHsl(0, 255, 0) }).toMatchSnapshot()
+  })
+
   it('should convert black', () => {
     expect({ background: rgbToHsl(0, 0, 0) }).toMatchSnapshot()
   })
