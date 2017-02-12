82ccc2a3d2e15f65980dfbbb29febe180e4455bc
Nik Graf <nik@nikgraf.com>
2017-02-12 19:32:25 +0100

    chore(darken): add an example

--- src/helpers/darken.js
+++ src/helpers/darken.js
@@ -6,7 +6,27 @@ import hslToHex from '../internalHelpers/_hslToHex'
 import hslToRgb from '../internalHelpers/_hslToRgb'
 
 /**
- * Darken
+ * Returns a string value for the darkened color.
+ *
+ * @example
+ * // Styles as object usage
+ * const styles = {
+ *   background: darken(0.2, '#FFCD64'),
+ *   background: darken(0.2, 'rgba(255,205,100,0.7)'),
+ * }
+ *
+ * // styled-components usage
+ * const div = styled.div`
+ *   background: ${darken(0.2, '#FFCD64')};
+ *   background: ${darken(0.2, 'rgba(255,205,100,0.7)')};
+ * `
+ *
+ * // CSS in JS Output
+ *
+ * element {
+ *   background: "#ffbd31";
+ *   background: "rgba(255,189,49,0.7)";
+ * }
  */
 function darken(amount: number, color: string): string {
   // parse color string to hsl
--- src/helpers/test/darken.test.js
+++ src/helpers/test/darken.test.js
@@ -6,7 +6,7 @@ describe('rgb', () => {
     expect(darken(0.2, '#444')).toMatchSnapshot()
   })
 
-  it('should darken a typical color with opacity by 10%', () => {
+  it('should darken a color with opacity by 10%', () => {
     expect(darken(0.1, 'rgba(220,140,140,0.7)')).toMatchSnapshot()
   })
 
