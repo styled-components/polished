8c11806fb81dd904d0695f1a18601a206645ac81
Martin Machycek <m.machycek@gmail.com>
2017-02-18 18:22:20 +1100

    chore(transparentize): Added isHsla regex

--- src/internalHelpers/_isValidColor.js
+++ src/internalHelpers/_isValidColor.js
@@ -2,3 +2,4 @@
 export const isRgb = (color: string) : bool => /rgb\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)/.test(color)
 export const isRgba = (color: string) : bool => /rgba\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3}), ?(\d{1})?.?(\d{1,2})\)/.test(color)
 export const isHsl = (color: string) : bool => /hsl\((\d{1,3}), ?(\d{1,3})%, ?(\d{1,3})%\)/.test(color)
+export const isHsla = (color: string) : bool => /hsla\((\d{1,3}), ?(\d{1,3})%, ?(\d{1,3})%, ?(\d{1})?.?(\d{1,2})\)/.test(color)
