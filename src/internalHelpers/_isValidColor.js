5f0c618da80e00273379b84fce306252185b879e
Martin Machycek <m.machycek@gmail.com>
2017-02-18 18:21:46 +1100

    chore(transparentize): Added isRgba regex

--- src/internalHelpers/_isValidColor.js
+++ src/internalHelpers/_isValidColor.js
@@ -1,2 +1,3 @@
 // @flow
 export const isRgb = (color: string) : bool => /rgb\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)/.test(color)
+export const isRgba = (color: string) : bool => /rgba\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3}), ?(\d{1})?.?(\d{1,2})\)/.test(color)
