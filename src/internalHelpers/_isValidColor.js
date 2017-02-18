df7e2d26cce06099e173861393902462ac8d1713
Martin Machycek <m.machycek@gmail.com>
2017-02-18 18:23:11 +1100

    chore(transparentize): Added isHex regex

--- src/internalHelpers/_isValidColor.js
+++ src/internalHelpers/_isValidColor.js
@@ -3,3 +3,4 @@ export const isRgb = (color: string) : bool => /rgb\((\d{1,3}), ?(\d{1,3}), ?(\d
 export const isRgba = (color: string) : bool => /rgba\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3}), ?(\d{1})?.?(\d{1,2})\)/.test(color)
 export const isHsl = (color: string) : bool => /hsl\((\d{1,3}), ?(\d{1,3})%, ?(\d{1,3})%\)/.test(color)
 export const isHsla = (color: string) : bool => /hsla\((\d{1,3}), ?(\d{1,3})%, ?(\d{1,3})%, ?(\d{1})?.?(\d{1,2})\)/.test(color)
+export const isHex = (color: string) : bool => /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color)
