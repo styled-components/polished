733b95ebd92a2125754fe71f13d1f4dd97ee51b2
Brian Hough <brian.r.hough@gmail.com>
2017-03-31 13:05:48 -0700

    chore(errors): Updated message handling

--- src/internalHelpers/test/_messageHandler.test.js
+++ src/internalHelpers/test/_messageHandler.test.js
@@ -3,53 +3,49 @@ import { error, warning } from '../_messageHandler'
 describe('messageHandler', () => {
   it('should properly print an error', () => {
     global.console = { log: jest.fn() }
-    const modulePath = 'mixins/normalize.js'
-    const messageBody = `normalize optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type ${typeof excludeOpinionated} instead.`
-    const docPath = '#normalize'
+    const messageBody = `optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type ${typeof excludeOpinionated} instead.`
 
-    const header = '%c -- error --------------------------------------------------- mixins/normalize.js -- '
+    const header = '%c -- error --------------------------------------------------- test/_messageHandler.test.js -- '
 
     const body = `%c
 
 
-normalize optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type undefined instead.
+_messageHandler.test %coptionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type undefined instead.
 
   `
 
     const info = `%c
-Please see the documentation at %chttps://www.polished.js.org/#normalize %cfor more information.
+Please see the documentation at %chttps://www.polished.js.org/#_messageHandler.test %cfor more information.
 
   `
     const message = `${header}${body}${info}`
 
-    error(modulePath, messageBody, docPath)
+    error(messageBody)
     // eslint-disable-next-line no-console
-    expect(console.log).toBeCalledWith(message, 'padding: 2px; font-weight: bold; background: red; color: black', 'color: black', 'color: slategray', 'color: blue', 'color: slategray')
+    expect(console.log).toBeCalledWith(message, 'padding: 2px; font-weight: bold; background: red; color: black', 'color: black', 'color: slategray; font-weight: bold', 'color: slategray', 'color: blue', 'color: slategray')
   })
 
-  it('should properly print an warning', () => {
+  it('should properly print a warning', () => {
     global.console = { log: jest.fn() }
-    const modulePath = 'mixins/normalize.js'
-    const messageBody = `normalize optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type ${typeof excludeOpinionated} instead.`
-    const docPath = '#normalize'
+    const messageBody = `optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type ${typeof excludeOpinionated} instead.`
 
-    const header = '%c -- warning --------------------------------------------------- mixins/normalize.js -- '
+    const header = '%c -- warning --------------------------------------------------- test/_messageHandler.test.js -- '
 
     const body = `%c
 
 
-normalize optionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type undefined instead.
+_messageHandler.test %coptionally takes a boolean that determines if opinionated rules should be included. However, you provided a parameter of type undefined instead.
 
   `
 
     const info = `%c
-Please see the documentation at %chttps://www.polished.js.org/#normalize %cfor more information.
+Please see the documentation at %chttps://www.polished.js.org/#_messageHandler.test %cfor more information.
 
   `
     const message = `${header}${body}${info}`
 
-    warning(modulePath, messageBody, docPath)
+    warning(messageBody)
     // eslint-disable-next-line no-console
-    expect(console.log).toBeCalledWith(message, 'padding: 2px; font-weight: bold; background: gold; color: black', 'color: black', 'color: slategray', 'color: blue', 'color: slategray')
+    expect(console.log).toBeCalledWith(message, 'padding: 2px; font-weight: bold; background: gold; color: black', 'color: black', 'color: slategray; font-weight: bold', 'color: slategray', 'color: blue', 'color: slategray')
   })
 })
