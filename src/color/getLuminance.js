ed140b2b6937aea49e3bf1ca444261ddb65eb3b6
James Quinlan <jamesquinlan@prometheus.local>
2017-05-03 19:28:19 -0400

    feat(getLuminance): Add getLuminance function

---
+++ src/color/getLuminance.js
@@ -0,0 +1,41 @@
+// @flow
+
+import parseToRgb from './parseToRgb'
+import curry from '../internalHelpers/_curry'
+
+/**
+ * Returns a number representing the luminance of a color.
+ *
+ * @example
+ * // Styles as object usage
+ * const styles = {
+ *   background: getLuminance('#CCCD64') >= getLuminance('#0000ff') ? '#CCCD64' : '#0000ff',
+ *   background: getLuminance('rgba(58, 133, 255, 1)') >= getLuminance('rgba(255, 57, 149, 1)') ?
+ *                             'rgba(58, 133, 255, 1)' :
+ *                             'rgba(255, 57, 149, 1)',
+ * }
+ *
+ * // styled-components usage
+ * const div = styled.div`
+ *   background: ${getLuminance('#CCCD64') >= getLuminance('#0000ff') ? '#CCCD64' : '#0000ff'};
+ *   background: ${getLuminance('rgba(58, 133, 255, 1)') >= getLuminance('rgba(255, 57, 149, 1)') ?
+ *                             'rgba(58, 133, 255, 1)' :
+ *                             'rgba(255, 57, 149, 1)'};
+ *
+ *
+ */
+function getLuminance(color: string): number {
+  const rgbColor = parseToRgb(color);
+  const [r, g, b] = Object.keys(rgbColor).map(key => {
+    const channel = rgbColor[key] / 255;
+    return channel <= 0.03928
+      ? channel / 12.92
+      : Math.pow((channel + 0.055) / 1.055, 2.4);
+  });
+  return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
+
+
+// Don’t inline this variable into export because Rollup will remove the /*#__PURE__*/ comment
+const curriedGetLuminance = /*#__PURE__*/ curry(getLuminance) // eslint-disable-line spaced-comment
+export default curriedGetLuminance
---
+++ src/color/test/getLuminance.test.js
@@ -0,0 +1,12 @@
+// @flow
+import getLuminance from '../getLuminance'
+
+describe('getLuminance', () => {
+  it('should return the luminance of a hex color', () => {
+    expect(getLuminance('#444')).toMatchSnapshot()
+  })
+
+  it('should return the luminance of an rgba color', () => {
+    expect(getLuminance('rgba(204,205,100,0.7)')).toMatchSnapshot()
+  })
+})
