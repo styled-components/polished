361e46f1bc1c258388721d26990a16451071c67d
Brian Hough <brian.r.hough@gmail.com>
2017-05-11 00:38:39 +0200

    fix(triangle): Complete triangle mixin (#162)
    Finishes the triangle mixin with an updated API to be more in line with Bourbon's implementation.
    
    Also updates the associated docs and properly exports the module.

--- src/mixins/triangle.js
+++ src/mixins/triangle.js
@@ -1,47 +1,17 @@
 // @flow
 
-
-/**
- * CSS to represent triangle with any pointing direction.
- *
- * @example
- * // Styles as object usage
- *
- * const styles = {
- *   ...triangle({ pointing: 'right', width: '100px', height: '100px', color: 'red' })
- * }
- *
- *
- * // styled-components usage
- * const div = styled.div`
- *   ${triangle({ pointing: 'right', width: '100px', height: '100px', color: 'red' })}
- *
- *
- * // CSS as JS Output
- *
- * div: {
- *  'border-color': 'transparent',
- *  'border-left-color': 'red !important',
- *  'border-style': 'solid',
- *  'border-width': '50px 0 50px 100px',
- *  'height': '0',
- *  'width': '0',
- * }
- */
-
+/** */
 type PointingDirection = 'top' | 'right' | 'bottom' | 'left'
 
-type BorderWidthArgs = {
-  height: number,
-  width: number,
+type TriangleArgs = {
+  backgroundColor?: string,
+  foregroundColor: string,
+  height: number|string,
+  width: number|string,
   pointingDirection: PointingDirection,
 }
 
-type TriangleArgs = BorderWidthArgs & {
-  color: string,
-}
-
-const getBorderWidth = ({ pointingDirection, height, width } : BorderWidthArgs) => {
+const getBorderWidth = (pointingDirection: PointingDirection, height: number, width: number) => {
   switch (pointingDirection) {
     case 'top': return `0 ${width / 2}px ${height}px ${width / 2}px`
     case 'left': return `${height / 2}px ${width}px ${height / 2}px 0`
@@ -60,20 +30,54 @@ const reverseDirection = {
   bottom: 'top',
 }
 
-const triangle = ({ pointingDirection, width, height, color } : TriangleArgs) => ({
-  'border-color': 'transparent',
-  'width': '0',
-  'height': '0',
-  'border-width': getBorderWidth({ height, width, pointingDirection }),
-  'border-style': 'solid',
+/**
+ * CSS to represent triangle with any pointing direction with an optional background color. Accepts number or px values for height and width.
+ *
+ * @example
+ * // Styles as object usage
+ *
+ * const styles = {
+ *   ...triangle({ pointingDirection: 'right', width: '100px', height: '100px', foregroundColor: 'red' })
+ * }
+ *
+ *
+ * // styled-components usage
+ * const div = styled.div`
+ *   ${triangle({ pointingDirection: 'right', width: '100px', height: '100px', foregroundColor: 'red' })}
+ *
+ *
+ * // CSS as JS Output
+ *
+ * div: {
+ *  'border-color': 'transparent',
+ *  'border-left-color': 'red !important',
+ *  'border-style': 'solid',
+ *  'border-width': '50px 0 50px 100px',
+ *  'height': '0',
+ *  'width': '0',
+ * }
+ */
 
-  /*
-  * javascript Object sorting orders 'border-color' after 'border-bottom-color'
-  * (bottom-b) is before (bottom-c) - !important is needed
-  * { border-bottom-color: 'red', border-color: 'transparent' }
-  */
+function triangle({ pointingDirection, height, width, foregroundColor, backgroundColor = 'transparent' } : TriangleArgs) {
+  const unitlessHeight = parseFloat(height)
+  const unitlessWidth = parseFloat(width)
+  if (isNaN(unitlessHeight) || isNaN(unitlessWidth)) {
+    throw new Error('Passed an invalid value to `height` or `width`. Please provide a pixel based unit')
+  }
 
-  [`border-${reverseDirection[pointingDirection]}-color`]: `${color} !important`,
-})
+  return {
+    'border-color': backgroundColor,
+    'width': '0',
+    'height': '0',
+    'border-width': getBorderWidth(pointingDirection, unitlessHeight, unitlessWidth),
+    'border-style': 'solid',
+    /*
+    * javascript Object sorting orders 'border-color' after 'border-bottom-color'
+    * (bottom-b) is before (bottom-c) - !important is needed
+    * { border-bottom-color: 'red', border-color: 'transparent' }
+    */
+    [`border-${reverseDirection[pointingDirection]}-color`]: `${foregroundColor} !important`,
+  }
+}
 
 export default triangle
