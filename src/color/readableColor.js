af0b2e83020aa555978127ff18569fed794bcde2
Dennis Walsh <lawjolla@aol.com>
2017-06-18 20:17:07 -0700

    test(color): Updated tests and reorganized code
    Added tests for hsl, hsla, rgba and named css colors.  Updated readableColor.js by extracting
    w3cConversion function from readableColo
    
    None

--- src/color/readableColor.js
+++ src/color/readableColor.js
@@ -3,38 +3,42 @@
 import parseToRgb from './parseToRgb'
 import curry from '../internalHelpers/_curry'
 
+const w3cConversion = (rgbColor: number): number => {
+  const colorZeroToOne = rgbColor / 255
+  return colorZeroToOne <= 0.03928
+    ? colorZeroToOne / 12.92
+    : ((colorZeroToOne + 0.055) / 1.055) ** 2.4
+}
+
 /**
  * Selects black or white or best contrast depending on the luminosity of the supplied color.
- * Follows W3C specs for readability
+ * Follows W3C specs for readability at https://www.w3.org/TR/WCAG20-TECHS/G18.html
  *
  * @example
  * // Styles as object usage
  * const styles = {
- *   color: mostReadable('#000'),
- *   color: mostReadable('rgb(255,0,0)'),
+ *   color: readableColor('#000'),
+ *   color: readableColor('papayawhip'),
+ *   color: readableColor('rgb(255,0,0)'),
  * }
  *
  * // styled-components usage
  * const div = styled.div`
- *   color: ${mostReadable('#000')};
- *   color: ${mostReadable('rgb(255,0,0)')};
+ *   color: ${readableColor('#000')};
+ *   color: ${readableColor('papayawhip')};
+ *   color: ${readableColor('rgb(255,0,0)')};
  * `
  *
  * // CSS in JS Output
  *
  * element {
  *   color: "#fff";
+ *   color: "#fff";
  *   color: "#000";
  * }
  */
 
 function readableColor(color: string): string {
-  const w3cConversion = (rgbColor: number): number => {
-    const colorZeroToOne = rgbColor / 255
-    return colorZeroToOne <= 0.03928
-      ? colorZeroToOne / 12.92
-      : ((colorZeroToOne + 0.055) / 1.055) ** 2.4
-  }
   const parsedColor = parseToRgb(color)
   const luminosity =
     w3cConversion(parsedColor.red) * 0.2126 +
--- src/color/test/readableColor.test.js
+++ src/color/test/readableColor.test.js
@@ -2,35 +2,59 @@
 import readableColor from '../readableColor'
 
 describe('readableColor', () => {
-  it('should return black given white', () => {
+  it('should return black given white, rgb(255,255,255)', () => {
     expect(readableColor('rgb(255,255,255)')).toMatchSnapshot()
   })
 
-  it('should return white given black', () => {
+  it('should return white given black, rgb(0,0,0)', () => {
     expect(readableColor('rgb(0,0,0)')).toMatchSnapshot()
   })
 
-  it('should return black given white hex', () => {
+  it('should return black given white hex, #fff', () => {
     expect(readableColor('#fff')).toMatchSnapshot()
   })
 
-  it('should return white given black hex', () => {
+  it('should return white given black, #000', () => {
     expect(readableColor('#000')).toMatchSnapshot()
   })
 
-  it('should return black given red', () => {
+  it('should return black given red, #FF0000', () => {
     expect(readableColor('#FF0000')).toMatchSnapshot()
   })
 
-  it('should return white given blue', () => {
+  it('should return white given blue, #0000FF', () => {
     expect(readableColor('#0000FF')).toMatchSnapshot()
   })
 
-  it('should return white given this gray', () => {
-    expect(readableColor('rgb(118,118,118)')).toMatchSnapshot()
+  it('should return black given rgb(120,120,120)', () => {
+    expect(readableColor('rgb(120,120,120)')).toMatchSnapshot()
   })
 
-  it('should return black given this gray', () => {
-    expect(readableColor('rgb(117,118,118)')).toMatchSnapshot()
+  it('should return white given rgb(117,117,117)', () => {
+    expect(readableColor('rgb(117,117,117)')).toMatchSnapshot()
+  })
+  it('should return white given black, rgba(0,0,0,0.7)', () => {
+    expect(readableColor('rgba(0,0,0,0.7)')).toMatchSnapshot()
+  })
+  it('should return white given black, rgba(0,0,0,0.1)', () => {
+    expect(readableColor('rgba(0,0,0,0.1)')).toMatchSnapshot()
+  })
+  it('should return black given white', () => {
+    expect(readableColor('white')).toMatchSnapshot()
+  })
+  it('should return black given papayawhip', () => {
+    expect(readableColor('papayawhip')).toMatchSnapshot()
+  })
+  it('should return black given white', () => {
+    expect(readableColor('white')).toMatchSnapshot()
+  })
+  it('should return black given red, hsl(0, 100%, 50%)', () => {
+    expect(readableColor('hsl(0, 100%, 50%)')).toMatchSnapshot()
+  })
+  it('should return white given blue, hsl(250, 100%, 50%)', () => {
+    expect(readableColor('hsl(250, 100%, 50%)')).toMatchSnapshot()
+  })
+  it('should return white given blue, hsla(250, 100%, 50%, 0.2)', () => {
+    expect(readableColor('hsla(250, 100%, 50%, 0.2)')).toMatchSnapshot()
   })
 })
