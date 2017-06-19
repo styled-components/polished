e95cbe39b125c058ae4c4f80c862a1b38b0cadeb
D Walsh <dwalshSSD@Ds-Mac-Pro-2.local>
2017-06-19 12:06:18 -0700

    refactor(color): Compacted readableColor.  Added tests.  Added readableColor to .documentation.json
    Removed all unneeded variables from readableColor and helper function.  Added hsl and hex test for
    middle luminosity.  Added readableColor to documentation.json

--- src/color/readableColor.js
+++ src/color/readableColor.js
@@ -3,15 +3,11 @@
 import parseToRgb from './parseToRgb'
 import curry from '../internalHelpers/_curry'
 
-const w3cConversion = (rgbColor: number): number => {
-  const colorZeroToOne = rgbColor / 255
-  return colorZeroToOne <= 0.03928
-    ? colorZeroToOne / 12.92
-    : ((colorZeroToOne + 0.055) / 1.055) ** 2.4
-}
+const h = (c: number): number =>
+  c / 255 <= 0.03928 ? c / 255 / 12.92 : ((c / 255 + 0.055) / 1.055) ** 2.4
 
 /**
- * Selects black or white or best contrast depending on the luminosity of the supplied color.
+ * Selects black or white for best contrast depending on the luminosity of the given color.
  * Follows W3C specs for readability at https://www.w3.org/TR/WCAG20-TECHS/G18.html
  *
  * @example
@@ -39,12 +35,10 @@ const w3cConversion = (rgbColor: number): number => {
  */
 
 function readableColor(color: string): string {
-  const parsedColor = parseToRgb(color)
-  const luminosity =
-    w3cConversion(parsedColor.red) * 0.2126 +
-    w3cConversion(parsedColor.green) * 0.7152 +
-    w3cConversion(parsedColor.blue) * 0.0722
-  return luminosity > 0.179 ? '#000' : '#fff'
+  const c = parseToRgb(color)
+  return h(c.red) * 0.2126 + h(c.green) * 0.7152 + h(c.blue) * 0.0722 > 0.179
+    ? '#000'
+    : '#fff'
 }
 
 export default curry(readableColor)
--- src/color/test/readableColor.test.js
+++ src/color/test/readableColor.test.js
@@ -2,14 +2,6 @@
 import readableColor from '../readableColor'
 
 describe('readableColor', () => {
-  it('should return black given white, rgb(255,255,255)', () => {
-    expect(readableColor('rgb(255,255,255)')).toMatchSnapshot()
-  })
-
-  it('should return white given black, rgb(0,0,0)', () => {
-    expect(readableColor('rgb(0,0,0)')).toMatchSnapshot()
-  })
-
   it('should return black given white hex, #fff', () => {
     expect(readableColor('#fff')).toMatchSnapshot()
   })
@@ -21,11 +13,21 @@ describe('readableColor', () => {
   it('should return black given red, #FF0000', () => {
     expect(readableColor('#FF0000')).toMatchSnapshot()
   })
-
   it('should return white given blue, #0000FF', () => {
     expect(readableColor('#0000FF')).toMatchSnapshot()
   })
-
+  it('should return black given gray, #787878', () => {
+    expect(readableColor('#787878')).toMatchSnapshot()
+  })
+  it('should return white given gray, #757575', () => {
+    expect(readableColor('#757575')).toMatchSnapshot()
+  })
+  it('should return black given white, rgb(255,255,255)', () => {
+    expect(readableColor('rgb(255,255,255)')).toMatchSnapshot()
+  })
+  it('should return white given black, rgb(0,0,0)', () => {
+    expect(readableColor('rgb(0,0,0)')).toMatchSnapshot()
+  })
   it('should return black given rgb(120,120,120)', () => {
     expect(readableColor('rgb(120,120,120)')).toMatchSnapshot()
   })
@@ -39,13 +41,16 @@ describe('readableColor', () => {
   it('should return white given black, rgba(0,0,0,0.1)', () => {
     expect(readableColor('rgba(0,0,0,0.1)')).toMatchSnapshot()
   })
-  it('should return black given white', () => {
-    expect(readableColor('white')).toMatchSnapshot()
+  it('should return white given black, "black"', () => {
+    expect(readableColor('black')).toMatchSnapshot()
   })
-  it('should return black given papayawhip', () => {
+  it('should return black given papayawhip, "papayawhip"', () => {
     expect(readableColor('papayawhip')).toMatchSnapshot()
   })
-  it('should return black given white', () => {
+  it('should return black given palevioletred, "palevioletred"', () => {
+    expect(readableColor('palevioletred')).toMatchSnapshot()
+  })
+  it('should return black given white, "white"', () => {
     expect(readableColor('white')).toMatchSnapshot()
   })
   it('should return black given red, hsl(0, 100%, 50%)', () => {
@@ -53,6 +58,12 @@ describe('readableColor', () => {
   })
   it('should return white given blue, hsl(250, 100%, 50%)', () => {
     expect(readableColor('hsl(250, 100%, 50%)')).toMatchSnapshot()
+  })
+  it('should return black given gray, hsl(0, 0%, 47%)', () => {
+    expect(readableColor('hsl(0, 0%, 47%)')).toMatchSnapshot()
+  })
+  it('should return white given gray, hsl(0, 0%, 45%)', () => {
+    expect(readableColor('hsl(0, 0%, 45%)')).toMatchSnapshot()
   })
   it('should return white given blue, hsla(250, 100%, 50%, 0.2)', () => {
     expect(readableColor('hsla(250, 100%, 50%, 0.2)')).toMatchSnapshot()
