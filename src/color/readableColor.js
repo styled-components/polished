70794c507f0d5309acf043fb6b25d1739b8fdcf0
Brian Hough <brian.r.hough@gmail.com>
2017-08-26 10:43:59 -0700

    feat(Typescript): Typescript Defs and Flow Improvements (#236)
    * feat(Typescript): Typescript Defs and Flow Improvements
    
    * feat(Typescript): Add readableColor to Typescript tests
    
    * fix(Docs): Rebuild docs

--- src/color/readableColor.js
+++ src/color/readableColor.js
@@ -1,5 +1,4 @@
 // @flow
-
 import parseToRgb from './parseToRgb'
 import curry from '../internalHelpers/_curry'
 
--- src/helpers/rem.js
+++ src/helpers/rem.js
@@ -27,5 +27,8 @@ import pixelsto from '../internalHelpers/_pxto'
  */
 
 // Don’t inline this variable into export because Rollup will remove the /*#__PURE__*/ comment
-const rem = /*#__PURE__*/ pixelsto('rem') // eslint-disable-line spaced-comment
+const rem: (
+  value: string | number,
+  base?: string | number,
+) => string = /*#__PURE__*/ pixelsto('rem') // eslint-disable-line spaced-comment
 export default rem
