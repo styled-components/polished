f6b1e4f6f9a8ea1057eb10b9e141183c15bed7d9
Tobias Lengsholz <tobias.lengsholz@invision.de>
2017-11-17 16:57:22 +0100

    Add tests for baseValue being a number

--- src/helpers/test/calc.test.js
+++ src/helpers/test/calc.test.js
@@ -8,17 +8,17 @@ const performCalculations = (unit: string) =>
       const expected = `14${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber + 2)).toBe(expected)
     })
-    it(`should subtract ${unit}  values`, () => {
+    it(`should subtract ${unit} values`, () => {
       const base = `12${unit}`
       const expected = `10${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber - 2)).toBe(expected)
     })
-    it(`should multiply ${unit}  values`, () => {
+    it(`should multiply ${unit} values`, () => {
       const base = `12${unit}`
       const expected = `24${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber * 2)).toBe(expected)
     })
-    it(`should divide ${unit}  values`, () => {
+    it(`should divide ${unit} values`, () => {
       const base = `12${unit}`
       const expected = `6${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber / 2)).toBe(expected)
@@ -28,4 +28,27 @@ const performCalculations = (unit: string) =>
 const units = ['px', '%', 'em', 'rem', 'ch', 'vw', 'vh', 'vmin', 'vmax']
 describe('calc', () => {
   units.map(performCalculations)
+
+  describe('without unit', () => {
+    it('should add values', () => {
+      const base = 12
+      const expected = 14
+      expect(calc(base, baseAsNumber => baseAsNumber + 2)).toBe(expected)
+    })
+    it('should subtract values', () => {
+      const base = 12
+      const expected = 10
+      expect(calc(base, baseAsNumber => baseAsNumber - 2)).toBe(expected)
+    })
+    it('should multiply values', () => {
+      const base = 12
+      const expected = 24
+      expect(calc(base, baseAsNumber => baseAsNumber * 2)).toBe(expected)
+    })
+    it('should divide values', () => {
+      const base = 12
+      const expected = 6
+      expect(calc(base, baseAsNumber => baseAsNumber / 2)).toBe(expected)
+    })
+  })
 })
