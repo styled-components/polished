1b244cae8e7fac5dd9ed9f444595058d392c6a9d
Tobias Lengsholz <tobias.lengsholz@invision.de>
2017-11-17 16:44:01 +0100

    Go a bit crazy in the unit tests

--- src/helpers/test/calc.test.js
+++ src/helpers/test/calc.test.js
@@ -1,27 +1,31 @@
 // @flow
 import calc from '../calc'
 
-describe('calc', () => {
-  describe('px', () => {
-    it('should add pixel values', () => {
-      const base = '12px'
-      const expected = '14px'
+const performCalculations = (unit: string) =>
+  describe(unit, () => {
+    it(`should add ${unit} values`, () => {
+      const base = `12${unit}`
+      const expected = `14${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber + 2)).toBe(expected)
     })
-    it('should subtract pixel values', () => {
-      const base = '12px'
-      const expected = '10px'
+    it(`should subtract ${unit}  values`, () => {
+      const base = `12${unit}`
+      const expected = `10${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber - 2)).toBe(expected)
     })
-    it('should multiply pixel values', () => {
-      const base = '12px'
-      const expected = '24px'
+    it(`should multiply ${unit}  values`, () => {
+      const base = `12${unit}`
+      const expected = `24${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber * 2)).toBe(expected)
     })
-    it('should divide pixel values', () => {
-      const base = '12px'
-      const expected = '6px'
+    it(`should divide ${unit}  values`, () => {
+      const base = `12${unit}`
+      const expected = `6${unit}`
       expect(calc(base, baseAsNumber => baseAsNumber / 2)).toBe(expected)
     })
   })
+
+const units = ['px', '%', 'em', 'rem', 'ch', 'vw', 'vh', 'vmin', 'vmax']
+describe('calc', () => {
+  units.map(performCalculations)
 })
