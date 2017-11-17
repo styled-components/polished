0465af6d4904924456c0ef7c474f157dbc8bb6de
Tobias Lengsholz <tobias.lengsholz@invision.de>
2017-11-17 16:36:41 +0100

    Add more test cases for pixel calculations

--- src/helpers/test/calc.test.js
+++ src/helpers/test/calc.test.js
@@ -2,9 +2,26 @@
 import calc from '../calc'
 
 describe('calc', () => {
-  it('should multiply pixel values', () => {
-    const base = '12px'
-    const expected = '24px'
-    expect(calc(base, baseAsNumber => baseAsNumber * 2)).toBe(expected)
+  describe('px', () => {
+    it('should add pixel values', () => {
+      const base = '12px'
+      const expected = '14px'
+      expect(calc(base, baseAsNumber => baseAsNumber + 2)).toBe(expected)
+    })
+    it('should subtract pixel values', () => {
+      const base = '12px'
+      const expected = '10px'
+      expect(calc(base, baseAsNumber => baseAsNumber - 2)).toBe(expected)
+    })
+    it('should multiply pixel values', () => {
+      const base = '12px'
+      const expected = '24px'
+      expect(calc(base, baseAsNumber => baseAsNumber * 2)).toBe(expected)
+    })
+    it('should divide pixel values', () => {
+      const base = '12px'
+      const expected = '6px'
+      expect(calc(base, baseAsNumber => baseAsNumber / 2)).toBe(expected)
+    })
   })
 })
