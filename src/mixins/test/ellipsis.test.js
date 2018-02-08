e035d106e1442d238cf53ca659fd478e4599bfa6
Brian Hough <brian.r.hough@gmail.com>
2018-02-08 14:47:28 -0800

    fix(ellipsis): Harden ellipsis mixin. (#298)
    Add `display: inline-flex` for browsers that support it better handle white-space issues with
    `display: inline-block`
    
    Addresses #256

--- src/mixins/test/ellipsis.test.js
+++ src/mixins/test/ellipsis.test.js
@@ -3,14 +3,14 @@ import ellipsis from '../ellipsis'
 
 describe('ellipsis', () => {
   it('should pass parameter to the value of max-width', () => {
-    expect({ ...ellipsis('300px') }).toMatchSnapshot()
+    expect(ellipsis('300px')).toMatchSnapshot()
   })
 
   it('should pass parameter of type integer to the value of max-width', () => {
-    expect({ ...ellipsis(300) }).toMatchSnapshot()
+    expect(ellipsis(300)).toMatchSnapshot()
   })
 
   it('should default max-width to 100%', () => {
-    expect({ ...ellipsis() }).toMatchSnapshot()
+    expect(ellipsis()).toMatchSnapshot()
   })
 })
