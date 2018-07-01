2850369bad5b199bfe9730996a5bb11fce12cc27
Brian Hough <brian.r.hough@gmail.com>
2018-07-01 14:42:31 -0700

    fix(mix): Fix first param issues with mix. (#327)
    Introducing a breaking change with mix that now requires the weight in order to function. The
    previous version that tried to make the first param optional with a fallback broke with Babel
    compilation and currying, and also did not provide the original intended API.
    
    BREAKING CHANGE: Mix no longer has a default weight, it must always be provided.

--- src/color/test/mix.test.js
+++ src/color/test/mix.test.js
@@ -2,10 +2,6 @@
 import mix from '../mix'
 
 describe('mix', () => {
-  it('should mix two colors', () => {
-    expect(mix(undefined, '#f00', '#00f')).toMatchSnapshot()
-  })
-
   it('should mix two colors with by a weight of 25%', () => {
     expect(mix(0.25, '#f00', '#00f')).toMatchSnapshot()
   })
