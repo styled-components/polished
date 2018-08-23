1b41407f22982ea84195c06d49a460881043cd0d
Bogdan Chadkin <trysound@yandex.ru>
2018-08-23 10:19:14 +0300

    Simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,63 +1,27 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
-import commonjs from 'rollup-plugin-commonjs'
-import inject from 'rollup-plugin-inject'
-import babel from 'rollup-plugin-babel'
-import json from 'rollup-plugin-json'
-import flow from 'rollup-plugin-flow'
-import { uglify } from 'rollup-plugin-uglify'
+import babel from "rollup-plugin-babel";
+import { uglify } from "rollup-plugin-uglify";
 
-const processShim = '\0process-shim'
+const input = "src/index.js";
+const name = "polished";
+const babelOptions = { plugins: ["external-helpers"] };
 
-const prod = process.env.PRODUCTION
-const mode = prod ? 'production' : 'development'
-
-console.log(`Creating ${mode} bundle...`)
-
-const output = prod ?
-[
-  { file: 'dist/polished.min.js', format: 'umd', name: 'polished', exports: 'named' },
-] :
-[
-  { file: 'dist/polished.js', format: 'umd', name: 'polished', exports: 'named' },
-  { file: 'dist/polished.es.js', format: 'es', name: 'polished', exports: 'named' },
-]
-
-const plugins = [
-  // Unlike Webpack and Browserify, Rollup doesn't automatically shim Node
-  // builtins like `process`. This ad-hoc plugin creates a 'virtual module'
-  // which includes a shim containing just the parts the bundle needs.
+export default [
   {
-    resolveId(importee) {
-      if (importee === processShim) return importee
-      return null
-    },
-    load(id) {
-      if (id === processShim) return 'export default { argv: [], env: {} }'
-      return null
-    },
-  },
-  flow(),
-  nodeResolve(),
-  commonjs(),
-  replace({
-    'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
-  }),
-  inject({
-    process: processShim,
-  }),
-  babel({
-    plugins: [
-      'external-helpers',
+    input,
+    output: [
+      { file: "dist/polished.js", format: "umd", name, exports: "named" },
+      { file: "dist/polished.es.js", format: "es" }
     ],
-  }),
-  json(),
-]
-
-if (prod) plugins.push(uglify())
-
-export default {
-  input: 'src/index.js',
-  output,
-  plugins,
-}
+    plugins: [babel(babelOptions)]
+  },
+  {
+    input,
+    output: {
+      file: "dist/polished.min.js",
+      format: "umd",
+      name,
+      exports: "named"
+    },
+    plugins: [babel(babelOptions), uglify()]
+  }
+];
