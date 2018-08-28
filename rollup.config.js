abc012198bd3880a6b4206b371c35143e9d9a4e7
Bogdan Chadkin <trysound@yandex.ru>
2018-08-28 11:08:42 +0300

    Upgrade to babel 7

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,29 @@
+import nodeResolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import replace from "rollup-plugin-replace";
 import { uglify } from "rollup-plugin-uglify";
 
 const input = "src/index.js";
 const name = "polished";
-const babelOptions = { plugins: ["external-helpers"] };
+const babelOptions = {
+  runtimeHelpers: true,
+  plugins: [["@babel/transform-runtime", { useESModules: true }]]
+};
 
 export default [
   {
     input,
     output: { file: "dist/polished.es.js", format: "es" },
+    // treat as external all node modules
+    external: id => !id.startsWith(".") && !id.startsWith("/"),
     plugins: [babel(babelOptions)]
   },
 
   {
     input,
-    output: { file: "dist/polished.js", format: "umd", name, exports: "named" },
+    output: { file: "dist/polished.js", format: "umd", name },
     plugins: [
+      nodeResolve(),
       babel(babelOptions),
       replace({ "process.env.NODE_ENV": JSON.stringify("development") })
     ]
@@ -24,13 +31,9 @@ export default [
 
   {
     input,
-    output: {
-      file: "dist/polished.min.js",
-      format: "umd",
-      name,
-      exports: "named"
-    },
+    output: { file: "dist/polished.min.js", format: "umd", name },
     plugins: [
+      nodeResolve(),
       babel(babelOptions),
       replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
       uglify()
