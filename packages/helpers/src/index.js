464089dacfec7b4c405aaab58c5c6b57829f9329
Brian Hough <brian.r.hough@gmail.com>
2018-09-07 18:56:19 -0700

    chore(Repo): More scoped package configuration

---
+++ packages/helpers/src/index.js
@@ -0,0 +1,16 @@
+// @flow
+import directionalProperty from './directionalProperty'
+import em from './em'
+import getValueAndUnit from './getValueAndUnit'
+import modularScale from './modularScale'
+import rem from './rem'
+import stripUnit from './stripUnit'
+
+export {
+  directionalProperty,
+  em,
+  getValueAndUnit,
+  modularScale,
+  rem,
+  stripUnit,
+}
---
+++ packages/shorthands/src/index.js
@@ -0,0 +1,34 @@
+// @flow
+import animation from './animation'
+import backgroundImages from './backgroundImages'
+import backgrounds from './backgrounds'
+import border from './border'
+import borderColor from './borderColor'
+import borderRadius from './borderRadius'
+import borderStyle from './borderStyle'
+import borderWidth from './borderWidth'
+import buttons from './buttons'
+import margin from './margin'
+import padding from './padding'
+import position from './position'
+import size from './size'
+import textInputs from './textInputs'
+import transitions from './transitions'
+
+export {
+  animation,
+  backgroundImages,
+  backgrounds,
+  border,
+  borderColor,
+  borderRadius,
+  borderStyle,
+  borderWidth,
+  buttons,
+  margin,
+  padding,
+  position,
+  size,
+  textInputs,
+  transitions,
+}
