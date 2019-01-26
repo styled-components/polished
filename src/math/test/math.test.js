8e0185903fbdd7c1578551337e6b17298b0155f7
Brian Hough <brian.r.hough@gmail.com>
2019-01-25 22:36:13 -0500

    chore(Math): Better math tests
    More exhaustive math tests

--- src/math/test/math.test.js
+++ src/math/test/math.test.js
@@ -2,42 +2,84 @@ import math from '../math'
 
 describe('math', () => {
   it('should be able to do simple addition', () => {
-    expect(math('1rem + 2rem')).toMatchSnapshot()
-    expect(math('1rem + 2')).toMatchSnapshot()
-    expect(math('1em + 5em')).toMatchSnapshot()
-    expect(math('1in + 5in + 10')).toMatchSnapshot()
+    expect(math('1rem + 2rem')).toEqual(`${1 + 2}rem`)
+    expect(math('1rem + 2')).toEqual(`${1 + 2}rem`)
+    expect(math('1em + 5em')).toEqual(`${1 + 5}em`)
+    expect(math('1em + -5em')).toEqual(`${1 + -5}em`)
+    expect(math('1in + 5in + 10')).toEqual(`${1 + 5 + 10}in`)
   })
+
   it('should be able to do simple subtraction', () => {
-    expect(math('1rem - 2rem')).toMatchSnapshot()
-    expect(math('1rem - 2')).toMatchSnapshot()
-    expect(math('1em - 5em')).toMatchSnapshot()
-    expect(math('1in - 5in - 10')).toMatchSnapshot()
+    expect(math('1rem - 2rem')).toEqual(`${1 - 2}rem`)
+    expect(math('1rem - 2')).toEqual(`${1 - 2}rem`)
+    expect(math('1em - 5em')).toEqual(`${1 - 5}em`)
+    expect(math('1em - -5em')).toEqual(`${1 - -5}em`)
+    expect(math('1in - 5in - 10')).toEqual(`${1 - 5 - 10}in`)
   })
+
   it('should be able to do simple multiplication', () => {
-    expect(math('1rem * 2rem')).toMatchSnapshot()
-    expect(math('1rem * 2')).toMatchSnapshot()
-    expect(math('1em * 5em')).toMatchSnapshot()
-    expect(math('1in * 5in * 10')).toMatchSnapshot()
+    expect(math('1rem * 2rem')).toEqual(`${1 * 2}rem`)
+    expect(math('1rem * 2')).toEqual(`${1 * 2}rem`)
+    expect(math('1em * 5em')).toEqual(`${1 * 5}em`)
+    expect(math('1em * -5em')).toEqual(`${1 * -5}em`)
+    expect(math('1in * 5in * 10')).toEqual(`${1 * 5 * 10}in`)
   })
+
   it('should be able to do simple division', () => {
-    expect(math('1rem * 2rem')).toMatchSnapshot()
-    expect(math('1rem * 2')).toMatchSnapshot()
-    expect(math('1em * 5em')).toMatchSnapshot()
-    expect(math('1in * 5in * 10')).toMatchSnapshot()
+    expect(math('1rem / 2rem')).toEqual(`${1 / 2}rem`)
+    expect(math('1rem / 2')).toEqual(`${1 / 2}rem`)
+    expect(math('1em / 5em')).toEqual(`${1 / 5}em`)
+    expect(math('1em / -5em')).toEqual(`${1 / -5}em`)
+    expect(math('1in / 5in / 10')).toEqual(`${1 / 5 / 10}in`)
   })
+
+  it('should be able to do simple min', () => {
+    expect(math('min(3em, 4em, 1em, 2em)')).toEqual(`${Math.min(3, 4, 1, 2)}em`)
+    expect(math('min(3em, -4em, 1em, 2em)')).toEqual(
+      `${Math.min(3, -4, 1, 2)}em`,
+    )
+  })
+
+  it('should be able to do simple max', () => {
+    expect(math('max(3em, 8em, 1em, 2em)')).toEqual(`${Math.max(3, 8, 1, 2)}em`)
+    expect(math('max(3em, -8em, 1em, 2em)')).toEqual(
+      `${Math.max(3, -8, 1, 2)}em`,
+    )
+  })
+
   it('should be able to do simple factorial', () => {
     expect(math('3em!')).toMatchSnapshot()
+    expect(math('171em!')).toMatchSnapshot()
+    expect(math('0px!')).toMatchSnapshot()
+    expect(math('-0.5px!')).toMatchSnapshot()
+    expect(math('-5px!')).toMatchSnapshot()
   })
-  it('should be able to do simple square root', () => {
-    expect(math('sqrt(4em)')).toMatchSnapshot()
+
+  it('should be able to process square root', () => {
+    expect(math('sqrt(4em)')).toEqual(`${Math.sqrt(4)}em`)
+    expect(math('sqrt(-4em)')).toEqual(`${Math.sqrt(-4)}em`)
+    expect(math('sqrt(2em + 4em)')).toEqual(`${Math.sqrt(2 + 4)}em`)
+    expect(math('sqrt(4em / 2em)')).toEqual(`${Math.sqrt(4 / 2)}em`)
+    expect(math('sqrt(4em + 2em * 5)')).toEqual(`${Math.sqrt(4 + 2 * 5)}em`)
+    expect(math('sqrt(4em - 2 / 5em)')).toEqual(`${Math.sqrt(4 - 2 / 5)}em`)
   })
-  it('should be able to do simple exponent power', () => {
-    expect(math('2em^3')).toMatchSnapshot()
+
+  it('should be able to process exponent power', () => {
+    expect(math('2em^3')).toEqual(`${2 ** 3}em`)
   })
-  it('should be able to do simple min', () => {
-    expect(math('min(3em, 4em, 1em, 2em)')).toMatchSnapshot()
+
+  it('should be able to process parentheses', () => {
+    expect(math('(1rem + 2rem) * 5')).toEqual(`${(1 + 2) * 5}rem`)
+    expect(math('(4em + 2)  * 5em + sqrt(4em - 2 / 5em)')).toEqual(
+      `${(4 + 2) * 5 + Math.sqrt(4 - 2 / 5)}em`,
+    )
   })
-  it('should be able to do simple max', () => {
-    expect(math('max(3em, 8em, 1em, 2em)')).toMatchSnapshot()
+
+  it('should throw an error when formula contains multiple units', () => {
+    expect(() => {
+      math('1rem + 1px')
+    }).toThrow(
+      'All values in a formula must have the same unit or be unitless.',
+    )
   })
 })
