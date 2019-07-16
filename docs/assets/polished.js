e64a437002ac3d051aa547b5d235cf42cb6bf689
Aaron Petcoff <hello@aaronpetcoff.me>
2019-07-15 22:01:14 -0400

    fix(hideVisually): remove `clip-path` from `hideVisually`
    The use of `clip-path` harms performance during scroll events in Chrome. In the meantime, browsers
    can fall back to using `clip` until this bug in Chrome is fixed.

--- docs/assets/polished.js
+++ docs/assets/polished.js
@@ -1242,10 +1242,15 @@
   }
 
   function hideVisually() {
+    // Note: The use of `clip-path` causes performance degredation on scroll events in Chrome.
+    // For more information, see:
+    // * h5bp/html5-boilerplate#2021
+    // * zurb/foundation-sites#10914
+    // * twbs/bootstrap#24906
     return {
       border: '0',
       clip: 'rect(0 0 0 0)',
-      clipPath: 'inset(50%)',
+      // clipPath: 'inset(50%)',
       height: '1px',
       margin: '-1px',
       overflow: 'hidden',
