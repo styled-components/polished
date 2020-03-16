89ec03c204bc8bc158292feb200b7c16730aa15f
Brody McKee <mrmckeb@hotmail.com>
2020-03-16 11:36:52 +0200

    chore: make requested changes

--- src/color/readableColor.js
+++ src/color/readableColor.js
@@ -1,14 +1,15 @@
 // @flow
+import getContrast from './getContrast'
 import getLuminance from './getLuminance'
-import meetsContrastGuidelines from './meetsContrastGuidelines'
 
 /**
  * Returns black or white (or optional light and dark return colors) for best
  * contrast depending on the luminosity of the given color.
  *
- * Set `strict` to `true` to ensure that the return color always meets or
- * exceeds WCAG level AA or greater. In this case, black or white are used as
- * fallback colors.
+ * When passing custom return colors, set `strict` to `true` to ensure that the
+ * return color always meets or exceeds WCAG level AA or greater. If this test
+ * fails, the default return color (black or white) is returned in place of the
+ * custom return color.
  *
  * Follows [W3C specs for readability](https://www.w3.org/TR/WCAG20-TECHS/G18.html).
  *
@@ -30,7 +31,6 @@ import meetsContrastGuidelines from './meetsContrastGuidelines'
  * `
  *
  * // CSS in JS Output
- *
  * element {
  *   color: "#fff";
  *   color: "#ff8";
@@ -44,20 +44,16 @@ const defaultDarkReturnColor = '#fff'
 
 export default function readableColor(
   color: string,
-  lightReturnColor: string = defaultLightReturnColor,
-  darkReturnColor: string = defaultDarkReturnColor,
-  strict: boolean = false,
+  lightReturnColor?: string = defaultLightReturnColor,
+  darkReturnColor?: string = defaultDarkReturnColor,
+  strict?: boolean = false,
 ): string {
   const isLightColor = getLuminance(color) > 0.179
   const preferredReturnColor = isLightColor ? lightReturnColor : darkReturnColor
 
-  // May return a color that does not meet WCAG AA.
   // TODO: Make `strict` the default behaviour in the next major release.
-  if (!strict) {
-    return preferredReturnColor
-  }
-
-  if (meetsContrastGuidelines(color, preferredReturnColor).AA) {
+  // Without `strict`, this may return a color that does not meet WCAG AA.
+  if (!strict || getContrast(color, preferredReturnColor) >= 4.5) {
     return preferredReturnColor
   }
   return isLightColor ? defaultLightReturnColor : defaultDarkReturnColor
