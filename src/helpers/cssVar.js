58c682dc2873ff346cfe7069ca9790c31e1333cf
Brian Hough <brian.r.hough@gmail.com>
2020-03-19 15:56:46 -0400

    chore(cssvar): fix error testing

--- src/helpers/cssVar.js
+++ src/helpers/cssVar.js
@@ -3,19 +3,41 @@ import PolishedError from '../internalHelpers/_errors'
 
 const cssVariableRegex = /--[\S]*/g
 
-// @private
+/**
+ * Fetches the value of a CSS Variable.
+ *
+ * @example
+ * // Styles as object usage
+ * const styles = {
+ *   '--dimension': stripUnit('100px'),
+ *   '--unit': stripUnit('100px')[1],
+ * }
+ *
+ * // styled-components usage
+ * const div = styled.div`
+ *   --dimension: ${stripUnit('100px')};
+ *   --unit: ${stripUnit('100px')[1]};
+ * `
+ *
+ * // CSS in JS Output
+ *
+ * element {
+ *   '--dimension': 100,
+ *   '--unit': 'px',
+ * }
+ */
 export default function cssVar(cssVariable: string): any {
   if (!cssVariable || !cssVariable.match(cssVariableRegex)) {
     throw new PolishedError(73)
   }
   // eslint-disable-next-line no-undef
-  const parsedCSSVariable = getComputedStyle(
+  const variableValue = getComputedStyle(
     // eslint-disable-next-line no-undef
     document.documentElement,
   ).getPropertyValue(cssVariable)
 
-  if (parsedCSSVariable) {
-    return parsedCSSVariable
+  if (variableValue) {
+    return variableValue
   } else {
     throw new PolishedError(74)
   }
