2095f2630551458d691205af6e6b69d6c991a344
Brian Hough <brian.r.hough@gmail.com>
2020-03-19 21:46:35 -0400

    fix(mix): fixed issue where mix was weighting the second alpha channel over the first
    Changed alpha calculation for mix to properly weight the first color's alpha value instead of the
    second.
    
    fix #477

--- src/color/test/mix.test.js
+++ src/color/test/mix.test.js
@@ -6,16 +6,22 @@ describe('mix', () => {
     expect(mix(0.25, '#f00', '#00f')).toMatchSnapshot()
   })
 
-  it('should mix two colors with an 8-digit hex color', () => {
+  it('should mix a color with an 8-digit hex color', () => {
     expect(mix(0.5, '#FF00007F', '#00f')).toMatchSnapshot()
   })
 
-  it('should mix two colors with a 4-digit hex color', () => {
+  it('should mix a 8-digit hex color with a 4-digit hex color', () => {
     expect(mix(0.5, '#FF00007F', '#0f08')).toMatchSnapshot()
   })
 
-  it('should mix two colors with opacity lower than 1', () => {
-    expect(mix(0.5, 'rgba(255, 0, 0, 0.5)', '#00f')).toMatchSnapshot()
+  it('should mix a color with a color with an opacity lower than 1', () => {
+    expect(mix(0.51, 'rgba(242, 236, 228, 0.5)', '#6b717f')).toMatchSnapshot()
+  })
+
+  it('should mix two rgba colors', () => {
+    expect(
+      mix(0.7, 'rgba(0, 0, 0, 1)', 'rgba(255, 255, 255, 0)'),
+    ).toMatchSnapshot()
   })
 
   it('should mix two colors when weight is a string', () => {
@@ -32,5 +38,11 @@ describe('mix', () => {
 
   it('should return transparent when passed transparent for both colors', () => {
     expect(mix('0.5', 'transparent', 'transparent')).toMatchSnapshot()
+  })
+
+  it('should return the second color when weight is 0', () => {
+    expect(
+      mix(0, 'rgba(0, 0, 0, 1)', 'rgba(255, 255, 255, 0)'),
+    ).toMatchSnapshot()
   })
 })
