075e4686b433fad7ab7bd8c162be96ccbbf3229c
Brian Hough <brian.r.hough@gmail.com>
2020-03-29 10:59:18 -0400

    build(babel): enable bugfix: true
    Enable bugfix:true in build and target explicit browser list

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,19 @@ const cjs = BABEL_ENV === "cjs" || NODE_ENV === "test";
 
 module.exports = {
   presets: [
-    ["@babel/env", { loose: true, exclude: [/transform-typeof-symbol/] }],
-    "@babel/flow"
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        exclude: [/transform-typeof-symbol/],
+        bugfixes: true,
+      },
+    ],
+    "@babel/flow",
   ],
   plugins: [
     cjs && "add-module-exports",
     "annotate-pure-calls",
-    "preval"
-  ].filter(Boolean)
+    "preval",
+  ].filter(Boolean),
 };
