ce2ede027e624904d4a460ba55a4738fe10d23ba
Brian Hough <brian.r.hough@gmail.com>
2020-03-29 10:34:34 -0400

    refactor(readablecolor): make strict mode default
    Make strict mode default when passing custom colors.

--- src/color/readableColor.js
+++ src/color/readableColor.js
@@ -8,10 +8,10 @@ const defaultDarkReturnColor = '#fff'
 /**
  * Returns black or white (or optional light and dark return colors) for best
  * contrast depending on the luminosity of the given color.
- * When passing custom return colors, set `strict` to `true` to ensure that the
+ * When passing custom return colors, strict mode ensures that the
  * return color always meets or exceeds WCAG level AA or greater. If this test
  * fails, the default return color (black or white) is returned in place of the
- * custom return color.
+ * custom return color. You can optionally turn off strict mode.
  *
  * Follows [W3C specs for readability](https://www.w3.org/TR/WCAG20-TECHS/G18.html).
  *
@@ -44,13 +44,11 @@ export default function readableColor(
   color: string,
   lightReturnColor?: string = defaultLightReturnColor,
   darkReturnColor?: string = defaultDarkReturnColor,
-  strict?: boolean = false,
+  strict?: boolean = true,
 ): string {
   const isLightColor = getLuminance(color) > 0.179
   const preferredReturnColor = isLightColor ? lightReturnColor : darkReturnColor
 
-  // TODO: Make `strict` the default behaviour in the next major release.
-  // Without `strict`, this may return a color that does not meet WCAG AA.
   if (!strict || getContrast(color, preferredReturnColor) >= 4.5) {
     return preferredReturnColor
   }
