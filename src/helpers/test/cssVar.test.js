cda8e3b16c18b86e03e6f81db97a4d0f94df6061
Brian Hough <brian.r.hough@gmail.com>
2020-05-11 18:39:46 -0400

    feat(cssvar): allow a default value for cssVar
    Allow the user to pass a default value to cssVar for when a variable is not found.

--- src/helpers/test/cssVar.test.js
+++ src/helpers/test/cssVar.test.js
@@ -10,32 +10,44 @@ beforeAll(() => {
   document.documentElement.style.setProperty('--our-background-color', 'red') // eslint-disable-line no-undef
   // $FlowFixMe
   document.documentElement.style.setProperty('--our-Background-Color', 'orange') // eslint-disable-line no-undef
+  // $FlowFixMe
+  document.documentElement.style.setProperty('--our-complex-value', '12px 12px') // eslint-disable-line no-undef
 })
 
 describe('cssVar', () => {
   test('gets a css variable', () => {
     expect(cssVar('--background')).toEqual('#FFF')
   })
+
   test('gets a hyphenated css variable', () => {
     expect(cssVar('--foreground-color')).toEqual('#000')
   })
+
   test('gets a complex hyphenated css variable', () => {
     expect(cssVar('--our-background-color')).toEqual('red')
   })
+
   test('respects casing', () => {
     expect(cssVar('--our-Background-Color')).toEqual('orange')
   })
-  test('errors when variable is not found', () => {
+
+  test('respects complex values', () => {
+    expect(cssVar('--our-complex-value')).toEqual('12px 12px')
+  })
+
+  test("returns default when variable isn't found.", () => {
+    expect(cssVar('--unfound-variable', 'orange')).toEqual('orange')
+  })
+
+  test('errors when variable is not found and no default is provided', () => {
     expect(() => {
       cssVar('--unfound-variable')
-    }).toThrow('CSS variable not found.')
+    }).toThrow('CSS variable not found and no default was provided.')
   })
+
   test('errors when variable is not formatted correctly', () => {
     expect(() => {
       cssVar('-bad-formatted-variable')
     }).toThrow('Please provide a valid CSS variable.')
-  })
-  test('passes value through when passthrough mode is enabled', () => {
-    expect(cssVar('#FFF', true)).toEqual('#FFF')
   })
 })
