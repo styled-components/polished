50c03255443364267ac4cb5a7db2f9762d97154a
Brian Hough <brian.r.hough@gmail.com>
2020-05-15 10:25:39 -0400

    fix(between): added error case when to/from unit is differs from mix/max (#511)
    * chore(library): update dependencies
    
    Update dependencies and fix new flow error in triangle.test.js
    
    * refactor(stripunit): fully deprecate returnUnit
    
    Fully deprecate returnUnit functionality and refactor return.
    
    * refactor(readablecolor): make strict mode default
    
    Make strict mode default when passing custom colors.
    
    * build(babel): enable bugfix: true
    
    Enable bugfix:true in build and target explicit browser list
    
    * build(lint-staged): remove superfulous git add
    
    * build(lerna): lerna init
    
    * feat(normalize): upgrade to normalize 8.0.1
    
    * build(docs): removed documentation.js auto generation
    
    * feat(fontface): now defaults to looking for local font first
    
    Default is now to look for a local font of the same family-name before downloading. Can be turned
    off by passing null to localFonts.
    
    BREAKING CHANGE: localFont will now be populated by default and may have unexpected behavior.
    
    * feat(important): important helper for module rule specificity
    
    important helper for targeting specific rules in module returns for adding `!important`-level
    specificity.
    
    * chore(important): update to not mutate original parameter
    
    * chore(important): add to index.js
    
    * fix(triangle): triangle now properly supports inherit
    
    triangle now returns properties in a way that allows inherit to work by declaring an individual side
    for the triangle color.
    
    * feat(cssvar): allow a default value for cssVar
    
    Allow the user to pass a default value to cssVar for when a variable is not found.
    
    * fix(between): added error case when to/from unit is different than min/max
    
    Added an error case when the unit for to/fromSize is different than min/maxScreen. This was causing
    bad calculations based on the differing scales.
    
    BREAKING CHANGE: If you were using a mix of unit and unitless measure or working with different
    units with similar scales (em/rem) you will now get an error where you may have been getting a
    seemingly valid calculation.
    
    fix #445

--- src/mixins/test/between.test.js
+++ src/mixins/test/between.test.js
@@ -10,13 +10,6 @@ describe('between', () => {
     expect(between('20px', '100px')).toMatchSnapshot()
   })
 
-  it('should return a valid calc formula when passed unitless to/from values as numbers', () => {
-    expect(between(20, 100)).toMatchSnapshot()
-  })
-
-  it('should return a valid calc formula when passed unitless to/from values as strings', () => {
-    expect(between('20', '100')).toMatchSnapshot()
-  })
   // Errors
   it('should throw an error when not passed min/max screen size as a string', () => {
     expect(() => {
@@ -42,6 +35,24 @@ describe('between', () => {
       between('1em', '100px', '400px', '1000px')
     }).toThrow(
       'fromSize and toSize must be provided as stringified numbers with the same units.',
+    )
+  })
+
+  it('should throw an error when passed to/from size with different units than mix/max screen', () => {
+    expect(() => {
+      // $FlowFixMe
+      between('1em', '100em', '400px', '1000px')
+    }).toThrow(
+      'fromSize and toSize must be provided as stringified numbers with the same units as minScreen and maxScreen.',
+    )
+  })
+
+  it('should throw an error when passed to/from size with no units but mix/max with units', () => {
+    expect(() => {
+      // $FlowFixMe
+      between(20, 100)
+    }).toThrow(
+      'fromSize and toSize must be provided as stringified numbers with the same units as minScreen and maxScreen.',
     )
   })
 })
